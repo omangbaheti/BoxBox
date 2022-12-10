0a9325957f25f643be6ae60ea572aba3d96aee2d
omangbaheti <60500898+omangbaheti@users.noreply.github.com>
2022-12-10 06:13:45 +0530

    Basic TileSystem

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -7,18 +7,18 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public Vector3 inputValues;
-    public Vector3 scaledAngle;
-
     [SerializeField] private Vector2 boxDimensions;
+    [SerializeField] private GameObject targetTile;
     private PlayerInput playerInput;
     private InputAction moveAction;
     private bool canMove = true;
-    [SerializeField] private List<Vector3> pivots;
+    [SerializeField] private CalculatePivots[] pivots;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log(transform.rotation * transform.localScale);
         SetInputActions();
     }
 
@@ -27,34 +27,48 @@ public class PlayerMovement : MonoBehaviour
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions["Move"];
         moveAction.performed += MoveActionOnPerformed;
+        
     }
 
     private void MoveActionOnPerformed(InputAction.CallbackContext input)
     {
         Vector2 inputVector = input.ReadValue<Vector2>();
-        Vector3 inputVector3D = new Vector3(inputVector.x, 0f, inputVector.y);
-        StartCoroutine(AnimateMove(inputVector3D));
+        Vector3 inputVector3D = new(inputVector.x, 0f, inputVector.y);
+        CheckValidity(inputVector3D);
+    }
+
+    private void CheckValidity(Vector3 input)
+    {
+        Ray downwardRay = new Ray(transform.position, Vector3.down);
+        Physics.Raycast(downwardRay, out var tileHit, 0.5f);
+        GameObject tile = tileHit.transform.gameObject;
+        
+        if (!tile.TryGetComponent(out Tile tileData)) return;
+
+        Debug.Log("Reached Here");
+        targetTile = tileData.CheckTargetTiles(gameObject, input);
+        if(targetTile == null) return;
+        StartCoroutine(AnimateMove(input));
     }
 
     private IEnumerator AnimateMove(Vector3 input)
     {
         if(!canMove) yield break;
         if(input.magnitude > 1) yield break;
-        transform.DOMove(transform.position + input, 0.1f);
-        yield return transform.DORotate(scaledAngle, 0.1f, RotateMode.WorldAxisAdd).SetRelative().WaitForCompletion();
+        canMove = false;
+        Vector3 targetPosition = new Vector3(targetTile.transform.position.x, transform.position.y, targetTile.transform.position.z);
+        transform.DOMove( targetPosition , 0.1f);
+        Vector3 newScale = new Vector3(transform.localScale.z, 0 ,transform.localScale.x);
+        transform.localScale = newScale;
+        //yield return transform.DORotate(rotationAngle, 0.1f, RotateMode.WorldAxisAdd).SetRelative().WaitForCompletion();
         canMove = true;
-
     }
     // Update is called once per frame
     void Update()
     {
         
     }
-
-    void OnMove(InputValue value)
-    {
-       
-    }
+    
 
     
 }
